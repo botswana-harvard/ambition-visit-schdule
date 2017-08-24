b7d8b7a44c0d21fe016e6eb4277ed7cf4e4dacb7
Leslie <leslie.kep@gmail.com>
2017-08-24 22:49:11 +0200

    add missing labs

--- ambition_visit_schedule/visit_schedules/requisitions.py
+++ ambition_visit_schedule/visit_schedules/requisitions.py
@@ -1,11 +1,11 @@
 from edc_visit_schedule import FormsCollection, Requisition
 
-from ambition_labs.labs import (viral_load_panel, cd4_panel, fbc_panel, csf_panel,
-                                chemistry_alt_panel, chemistry_panel)
+from ambition_labs.labs import viral_load_panel, cd4_panel, fbc_panel, csf_panel
+from ambition_labs.labs import chemistry_alt_panel, chemistry_panel, csf_chemistry_panel
 
 requisitions = FormsCollection(Requisition(
     show_order=50, model='ambition_subject.subjectrequisition',
-    panel=chemistry_alt_panel, required=False, additional=False),)
+    panel=chemistry_panel, required=False, additional=False),)
 
 requisitions_d1 = FormsCollection(
     Requisition(
@@ -22,6 +22,9 @@ requisitions_d1 = FormsCollection(
         panel=csf_panel, required=True, additional=False),
     Requisition(
         show_order=50, model='ambition_subject.subjectrequisition',
+        panel=csf_chemistry_panel, required=True, additional=False),
+    Requisition(
+        show_order=60, model='ambition_subject.subjectrequisition',
         panel=chemistry_alt_panel, required=True, additional=False),
 )
 
@@ -41,6 +44,9 @@ requisitions_d7 = FormsCollection(
     Requisition(
         show_order=30, model='ambition_subject.subjectrequisition',
         panel=csf_panel, required=True, additional=False),
+    Requisition(
+        show_order=40, model='ambition_subject.subjectrequisition',
+        panel=chemistry_alt_panel, required=True, additional=False),
 )
 
 requisitions_w4 = FormsCollection(
