88e3ed37181ed0bbf6373cf5c3d9a0c2bd7aeade
Leslie <leslie.kep@gmail.com>
2017-08-28 17:34:26 +0200

    update day 3 requisition

--- ambition_visit_schedule/visit_schedules/requisitions.py
+++ ambition_visit_schedule/visit_schedules/requisitions.py
@@ -38,7 +38,7 @@ requisitions_d1 = FormsCollection(
 requisitions_d3 = FormsCollection(
     Requisition(
         show_order=10, model='ambition_subject.subjectrequisition',
-        panel=chemistry_panel),
+        panel=chemistry_panel, required=True, additional=False),
 )
 
 requisitions_d7 = FormsCollection(
@@ -53,9 +53,6 @@ requisitions_d7 = FormsCollection(
         panel=csf_panel, required=True, additional=False),
     Requisition(
         show_order=40, model='ambition_subject.subjectrequisition',
-        panel=chemistry_alt_panel, required=True, additional=False),
-    Requisition(
-        show_order=50, model='ambition_subject.subjectrequisition',
         panel=plasma_panel, required=True, additional=False),
 )
 
